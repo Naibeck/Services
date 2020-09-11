39ad95db1c5c215f1998799a81ceb930766c3a32
Kevin Gomez <kg-diseno@outlook.es>
2020-09-11 19:07:52 -0300

    Step1: Initial project layout and instantiation

--- app/src/main/java/com/naibeck/services/MainActivity.kt
+++ app/src/main/java/com/naibeck/services/MainActivity.kt
@@ -2,10 +2,34 @@ package com.naibeck.services
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.naibeck.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        binding.foregroundServiceButton.setOnClickListener { launchService(Services.ForegroundService, binding.messageToService.text.toString().toInt()) }
+        binding.foregroundServiceButton.setOnClickListener { launchService(Services.IntentService, binding.messageToService.text.toString().toInt()) }
     }
+
+    private fun launchService(serviceType: Services, value: Int) {
+        when (serviceType) {
+            is Services.ForegroundService -> launchForegroundService(value)
+            is Services.IntentService -> launchIntentService(value)
+        }
+    }
+
+    private fun launchIntentService(value: Int) {
+        TODO("Not yet implemented")
+    }
+
+    private fun launchForegroundService(value: Int) {
+        TODO("Not yet implemented")
+    }
+}
+
+sealed class Services {
+    object ForegroundService : Services()
+    object IntentService : Services()
 }
