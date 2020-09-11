2184554557d4ca21147c5df019903d6a7362ba85
Kevin Gomez <kg-diseno@outlook.es>
2020-09-11 19:38:45 -0300

    Step5: Create notification and call startForeground in onStartCommand

--- app/src/main/java/com/naibeck/services/TimeCounterService.kt
+++ app/src/main/java/com/naibeck/services/TimeCounterService.kt
@@ -1,8 +1,12 @@
 package com.naibeck.services
 
+import android.app.Notification
+import android.app.PendingIntent
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import androidx.core.app.NotificationCompat
+import com.naibeck.services.ServicesApplication.Companion.CHANNEL_ID
 import timber.log.Timber
 
 class TimeCounterService : Service() {
@@ -13,7 +17,24 @@ class TimeCounterService : Service() {
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         Timber.d("Service started command")
+        val value: Int = intent?.getIntExtra(COUNTER, 0) ?: 0
+        startForeground(FOREGROUND_ID, createNotification(value))
+
         return START_NOT_STICKY
+    }
+
+    private fun createNotification(value: Int?): Notification? {
+        val intent = Intent(this, MainActivity::class.java)
+
+        val pendingIntent = PendingIntent.getActivity(this, FOREGROUND_REQUEST, intent, 0)
+
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Foreground Service")
+            .setContentText("This service contains $value. It will last until user terminates it.")
+            .setSmallIcon(R.drawable.ic_android)
+            .setContentIntent(pendingIntent)
+            .build()
+        return notification
     }
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -23,5 +44,11 @@ class TimeCounterService : Service() {
     override fun onDestroy() {
         super.onDestroy()
         Timber.d("Service destroyed")
+    }
+
+    companion object {
+        const val COUNTER = "counter.key"
+        const val FOREGROUND_REQUEST = 0
+        const val FOREGROUND_ID = 1
     }
 }
